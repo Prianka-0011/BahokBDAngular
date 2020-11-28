9bc5d71ef13266c0d0b43eed9f3d4e5abc00686d
pranka mondal <priankanew0011@gmail.com>
2020-11-29 00:01:02 +0600

    payment type start

--- BahokBDClient/ClientApp/src/app/user/admin/admin-routing.module.ts
+++ BahokBDClient/ClientApp/src/app/user/admin/admin-routing.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { PaymentTypeComponent } from './payment-type/payment-type.component';
 
 const routes: Routes = [{
   path: '', component: AdminComponent,
-  //children: [
-  //  { path: '', component: RegisterComponent },
-  //  { path: 'user', component: UserComponent },
-  //  { path: 'user/register', component: RegisterComponent },
-  //  { path: 'user/login', component: LoginComponent },
-  //  { path: 'user/test', component: TestComponent },
-  //  { path: 'user/admin', component: AdminComponent, canActivate: [AuthGuard] },
-  //  //{ path: 'user/admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)/*, canActivate: [AuthGuard]*/ }
-  //]
+  children: [
+    { path: 'admin', component: AdminComponent },
+    { path: 'admin/paytype', component: PaymentTypeComponent },
+    
+  ]
 }]
 
 @NgModule({
---
+++ BahokBDClient/ClientApp/src/app/user/admin/payment-type/payment-type.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-payment-type',
+    templateUrl: './payment-type.component.html',
+    styleUrls: ['./payment-type.component.scss']
+})
+/** payment-type component*/
+export class PaymentTypeComponent {
+    /** payment-type ctor */
+    constructor() {
+
+    }
+}
