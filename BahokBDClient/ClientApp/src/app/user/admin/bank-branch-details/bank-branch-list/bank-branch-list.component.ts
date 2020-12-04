d5bb746f4ac89fed3840af31a26f880c1f533209
pranka mondal <priankanew0011@gmail.com>
2020-12-04 23:59:38 +0600

    branch done

--- BahokBDClient/ClientApp/src/app/user/admin/bank-branch-details/bank-branch-list/bank-branch-list.component.ts
+++ BahokBDClient/ClientApp/src/app/user/admin/bank-branch-details/bank-branch-list/bank-branch-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BranchService } from '../branchshared/branch.service';
 import { ToastrService } from 'ngx-toastr';
+import { Branch } from '../branchshared/branch.model';
 
 @Component({
     selector: 'app-bank-branch-list',
@@ -16,5 +17,23 @@ export class BankBranchListComponent implements OnInit {
     }
     ngOnInit(): void {
       this.service.refreshList();
+  }
+  populateForm(pd: Branch) {
+    this.service.formData = Object.assign({}, pd);
+    console.log("this.service.formData", this.service.formData)
+  }
+  onDelete(Id) {
+    if (this.toastr.warning('Are you want to delete')) {
+      this.service.deletePaymentBankDetail(Id)
+        .subscribe(res => {
+          debugger;
+          this.service.refreshList();
+          this.toastr.warning('Deleted successfully', ' Branch Detail Register');
+        },
+          err => {
+            debugger;
+            console.log(err);
+          })
     }
+  }
 }
